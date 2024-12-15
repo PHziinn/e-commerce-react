d3bb45f6390d37a0cef0758ff74fbcffd872c6e8
PHziinn <skamikaze4@gmail.com>
2024-12-15 20:12:53 -0300

    feat: adicionando routes

--- src/routes/RoutesPath.js
+++ src/routes/RoutesPath.js
@@ -2,6 +2,9 @@ import { SignUp } from '../page/public/cadastrar';
 import { DetailsProducts } from '../page/public/detailsProduct';
 import { Home } from '../page/public/home';
 import { Login } from '../page/public/login';
+import { NotFound } from '../page/public/notFound';
+import { PaymentFailed } from '../page/public/paymentFailed';
+import { PaymentSuccess } from '../page/public/paymentSuccess';
 import { SearchProducts } from '../page/public/searchProduto';
 import { Unauthorized } from '../page/public/unauthorized';
 
@@ -16,4 +19,7 @@ export const RoutesPath = {
   '/login': Login,
   '/cadastrar': SignUp,
   '/unauthorized': Unauthorized,
+  '/paymentSuccess': PaymentSuccess,
+  '/paymentFailed': PaymentFailed,
+  '/*': NotFound,
 };
