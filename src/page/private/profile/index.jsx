50dea5fdea77c603930d375dac8b10b5d0c23177
PHziinn <skamikaze4@gmail.com>
2024-12-25 11:32:23 -0300

    feat: adicionando pagina de perfil

---
+++ src/page/private/profile/index.jsx
@@ -0,0 +1,26 @@
+import { Box, Container, useMediaQuery, useTheme } from '@mui/material';
+import { PrimarySearchBar } from '../../../components/navBarHeader';
+import { ProfileAccount } from '../../../components/profileAccount';
+import { Footer } from '../../../components/footer';
+
+export const Profile = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  return (
+    <>
+      <Container
+        maxWidth={false}
+        sx={{
+          maxWidth: isMobile ? '95vw' : '80vw',
+          paddingX: isMobile ? 0 : null,
+        }}>
+        <PrimarySearchBar showAppBar={false} />
+
+        <Box sx={{ marginTop: isMobile ? 15 : '3rem' }}>
+          <ProfileAccount />
+        </Box>
+      </Container>
+      <Footer />
+    </>
+  );
+};
