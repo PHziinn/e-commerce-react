a12c870c9404e2c0bf71b9fd4ddbb614b1b76168
PHziinn <skamikaze4@gmail.com>
2025-01-01 20:28:51 -0300

    chore: setting defaultOptions do reactQuery

--- src/service/ReactQueryProvider.jsx
+++ src/service/ReactQueryProvider.jsx
@@ -3,12 +3,12 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 5 * 1000, // 5 minutos de dados em cache antes de revalidar
-      cacheTime: 1800000, // Cache mantido por 30 minutos
-      refetchOnWindowFocus: false, // Evita refetch desnecessário ao focar na janela
+      staleTime: 5 * 60 * 1000,
+      cacheTime: 30 * 60 * 1000,
+      refetchOnWindowFocus: false,
     },
     mutations: {
-      retry: 2, // Tentativas automáticas de retrabalho para falhas
+      retry: 2,
     },
   },
 });
