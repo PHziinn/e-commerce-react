1b18c0bd65ee1391e5441838320b1cf8eee0550f
PHziinn <skamikaze4@gmail.com>
2025-01-04 17:34:57 -0300

    enhancement: Melhoria na exibição da pagina de manuntenção

--- src/page/public/maintenance/index.jsx
+++ src/page/public/maintenance/index.jsx
@@ -1,48 +1,59 @@
-import { Box, Typography, CircularProgress, Paper, Fade } from '@mui/material';
+import { Box, LinearProgress, Stack, Typography } from '@mui/material';
+import { FcServices } from 'react-icons/fc';
+import { FooterMaintenance } from './components/FooterMaintenance';
 
 export const MaintenancePage = () => {
   return (
-    <Box
-      component="main"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-        textAlign: 'center',
-        fontFamily: 'Arial',
-      }}>
-      <Typography sx={{ mb: 2, fontSize: '42px', fontWeight: 'bold' }}>
-        Estamos em Manutenção
-      </Typography>
-      <Typography
+    <Box component="main">
+      <Box
         sx={{
-          color: '#555555',
-          marginBottom: 4,
-          fontWeight: 400,
-          lineHeight: 1.6,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100vh',
+          textAlign: 'center',
+          fontFamily: 'Arial',
         }}>
-        Nosso site está em processo de melhorias. Pedimos desculpas pelo transtorno. Por favor,
-        volte mais tarde.
-      </Typography>
-      <CircularProgress
-        sx={{
-          color: '#3f51b5',
-          marginTop: 2,
-          animationDuration: '800ms',
-          marginBottom: 2,
-        }}
-      />
-      <Typography
-        variant="body2"
-        sx={{
-          color: '#999999',
-          fontSize: '0.875rem',
-          fontWeight: 300,
-        }}>
-        Agradecemos pela sua paciência.
-      </Typography>
+        <FcServices size={150} />
+        <Typography sx={{ mb: 2, fontSize: '42px', fontWeight: 'bold' }}>
+          Estamos em Manutenção
+        </Typography>
+
+        <Typography
+          sx={{
+            color: '#555555',
+            fontWeight: 400,
+            lineHeight: 1.6,
+          }}>
+          Nosso site está passando por manutenção neste momento.
+        </Typography>
+        <Typography
+          sx={{
+            color: '#555555',
+            marginBottom: 4,
+            fontWeight: 400,
+            lineHeight: 1.6,
+          }}>
+          Estamos trabalhando para melhorar a sua experiência.
+        </Typography>
+
+        <Stack
+          sx={{ width: '500px', mb: 3 }}
+          spacing={2}>
+          <LinearProgress color="inherit" />
+        </Stack>
+        <Typography
+          variant="body2"
+          sx={{
+            color: '#999999',
+            fontSize: '0.875rem',
+            fontWeight: 300,
+          }}>
+          Agradecemos pela sua paciência.
+        </Typography>
+      </Box>
+      <FooterMaintenance />
     </Box>
   );
 };
