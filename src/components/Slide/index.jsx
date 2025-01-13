ff1c54cbf1295591c096568f98f68472349e1360
PHziinn <skamikaze4@gmail.com>
2025-01-13 15:40:04 -0300

    feat: adicionando pagination no Slide

--- src/components/Slide/index.jsx
+++ src/components/Slide/index.jsx
@@ -3,7 +3,17 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import { Swiper } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
 
 export const SliderCard = ({ settings, children }) => {
-  return <Swiper {...settings}>{children}</Swiper>;
+  return (
+    <Swiper
+      {...settings}
+      style={{
+        '--swiper-pagination-color': '#000000',
+      }}
+      modules={[Pagination]}>
+      {children}
+    </Swiper>
+  );
 };
