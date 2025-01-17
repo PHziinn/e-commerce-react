57347aad09daa54cdf3eb7013a94ae990cbc4e31
PHziinn <skamikaze4@gmail.com>
2025-01-16 22:03:05 -0300

    chore: corrigindo conflito

--- src/page/public/home/index.jsx
+++ src/page/public/home/index.jsx
@@ -1,11 +1,11 @@
 import { Box, Container, useMediaQuery, useTheme } from '@mui/material';
 import { useQuery } from '@tanstack/react-query';
 import { useEffect } from 'react';
+import { CarouselTamplate } from '../../../components/CarouselTamplate';
 import { CardProducts } from '../../../components/cardProducts';
 import { Footer } from '../../../components/footer';
 import { PrimarySearchBar } from '../../../components/navBarHeader';
 import { getAllProdutos } from '../../../service/api';
-import { CarouselTamplate } from '../../../components/CarouselTamplate';
 
 export const Home = () => {
   const theme = useTheme();
